9127482c0912fa6d3f86f07beb5aa12092b15971
Mfon Eti-mfon <mfonetimfon@gmail.com>
2023-12-29 00:37:39 +0100

    Make OOO events fetching more robust
    * Fetch events that intersect with 'specified' time bounds
    * Don't fetch recurring events (fetch only single events)
    * Don't fetch cancelled events
    * Store event attendees -- in case event was created by rippling
      - filter out attendees that are the event creator
      - filter out attendees that are Calendars

--- src/anybodyhome.py
+++ src/anybodyhome.py
@@ -1,6 +1,7 @@
+import datetime
 import os
 import pathlib
-from datetime import datetime
+from datetime import datetime, timedelta, timezone
 from pprint import pprint
 
 from dotenv import load_dotenv
@@ -11,6 +12,8 @@ from googleapiclient.discovery import build
 from googleapiclient.errors import HttpError
 
 load_dotenv()
+
+UTC = timezone.utc
 
 
 def auth(scopes=None):
@@ -45,57 +48,77 @@ def auth(scopes=None):
 
 
 @auth(scopes=["https://www.googleapis.com/auth/calendar.readonly"])
-def fetch_eng_ooos(credentials=None):
+def fetch_eng_ooos(start_in=0, period=60 * 60, credentials=None):
+    # OOOs fetched will exclude those which:
+    # * end before `start_in` seconds from now
+    # * start after `start_in` + `period` seconds from now
+    time_min = datetime.now(UTC) + timedelta(seconds=start_in)
+    time_max = time_min + timedelta(period)
+
     try:
         service = build("calendar", "v3", credentials=credentials)
         page_token = None
-        page_count = 0
 
         ooos = []
         while True:
-            page_count += 1
-
             events = (
                 service.events()
                 .list(
                     calendarId=os.getenv("OOO_ENG_CALENDAR_ID"),
                     pageToken=page_token,
-                    maxResults=2,
+                    maxResults=10,
+                    showDeleted=False,
+                    singleEvents=True,
+                    orderBy="startTime",
+                    timeMin=time_min.isoformat(),
+                    timeMax=time_max.isoformat(),
                 )
                 .execute()
             )
 
             for event in events["items"]:
-                if event["status"] == "cancelled":
-                    continue
-
                 ooo = {"creator": event["creator"]["email"]}
+
+                if "attendees" in event:
+                    iter_attendees = (
+                        attendee["email"] for attendee in event["attendees"]
+                    )
+                    attendees = list(
+                        email
+                        for email in iter_attendees
+                        if (not email == ooo["creator"])
+                        and (not email.endswith("@group.calendar.google.com"))
+                    )
+
+                    if len(attendees) > 0:
+                        ooo["attendees"] = attendees
+
                 ooo["start"] = ooo_start = dict()
                 ooo["end"] = ooo_end = dict()
                 ooos.append(ooo)
 
                 event_start = event["start"]
-                start_date = event_start.get("date")
-                if start_date is not None:
-                    ooo_start["date"] = datetime.strptime(start_date, "%Y-%m-%d")
+                if "date" in event_start:
+                    ooo_start["date"] = datetime.strptime(
+                        event_start["date"], "%Y-%m-%d"
+                    )
                 else:
                     ooo_start["date_time"] = datetime.fromisoformat(
                         event_start["dateTime"]
                     )
-                    if timezone := event_start.get("timeZone") and timezone is not None:
-                        ooo_start["time_zone"] = timezone
+                    if "timezone" in event_start:
+                        ooo_start["time_zone"] = event_start["timezone"]
 
                 event_end = event["end"]
-                end_date = event_end.get("date")
-                if end_date is not None:
-                    ooo_end["date"] = datetime.strptime(end_date, "%Y-%m-%d")
+                if "date" in event_end:
+                    ooo_end["date"] = datetime.strptime(event_end["date"], "%Y-%m-%d")
                 else:
                     ooo_end["date_time"] = datetime.fromisoformat(event_end["dateTime"])
-                    if timezone := event_end.get("timeZone") and timezone is not None:
-                        ooo_end["time_zone"] = timezone
+                    if "timezone" in event_end:
+                        ooo_end["time_zone"] = event_end["timezone"]
 
             page_token = events.get("nextPageToken")
-            if not page_token or page_count >= 2:
+            if not page_token:
                 break
 
         return ooos
@@ -105,6 +128,7 @@ def fetch_eng_ooos(credentials=None):
 
 
 if __name__ == "__main__":
-    res = fetch_eng_ooos()
+    next_fourteen_days = 60 * 60 * 24 * 14
+    res = fetch_eng_ooos(period=next_fourteen_days)
     pprint("Fetched!")
     pprint(res)
